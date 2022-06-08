e2f2feb8d4e17dc0b09560d423373dcb53573879
wander <kai.wang@ziel.cn>
2022-06-08 19:10:39 +0800

    [feature]:物流看板添加告警

--- src/pages/Transport/v2/components/statistic_analysis.tsx
+++ src/pages/Transport/v2/components/statistic_analysis.tsx
@@ -1,7 +1,8 @@
+import { GetAlters } from '@/services/paasport/transport/v2/transport_v2_umirequest';
 import { RightOutlined } from '@ant-design/icons';
 import ProCard from '@ant-design/pro-card';
 import { Space, Typography } from 'antd';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'umi';
 
 type StatisticAnalysisReq = {
@@ -15,23 +16,36 @@ type StatisticAnalysisReq = {
 
 const StatisticAnalysis: React.FC<StatisticAnalysisReq> = (props) => {
     const { analysisReq, statisticTotal, exceptionsTotal, returningToSenderTotal, returnedToSenderTotal, deliveredTotal } = props;
+    const [warning, setWarning] = useState(0);
     const dateFormat = 'YYYY-MM-DD';
+    const commonConfig = {
+        hoverable: true,
+        style: { height: 150 }
+    }
+    useEffect(() => {
+        const getAlters = async () => {
+            const resp = await GetAlters({})
+            setWarning(resp.total)
+        }
+        getAlters();
+    }, [analysisReq])
     return (<ProCard ghost gutter={8} style={{ marginTop: 16 }} size='default'>
         <ProCard
-            hoverable
+            {...commonConfig}
             title={<Typography.Text type="secondary">Total</Typography.Text>}
+            layout='center'
             extra={<Link to={{
                 pathname: '/transport/v2/list',
                 search: `?begin_date=${analysisReq.begin_date.format(dateFormat)}&end_date=${analysisReq.end_date.format(dateFormat)}`
             }}><RightOutlined /></Link>}
         >
             <Space direction='vertical'>
-                <Typography.Text style={{ fontSize: 20 }}>{statisticTotal}</Typography.Text>
-                <Typography.Text type="secondary" style={{ visibility: 'hidden' }}> empty</Typography.Text>
+                <Typography.Title level={1}>{statisticTotal}</Typography.Title>
+                {/* <Typography.Text type="secondary" style={{ visibility: 'hidden' }}> empty</Typography.Text> */}
             </Space>
         </ProCard>
         <ProCard
-            hoverable
+            {...commonConfig}
             title={<Typography.Text type="secondary">EXCEPTION</Typography.Text>}
             extra={<Link to={{
                 pathname: '/transport/v2/list',
@@ -43,9 +57,23 @@ const StatisticAnalysis: React.FC<StatisticAnalysisReq> = (props) => {
                 <Typography.Text type="secondary">{(exceptionsTotal / statisticTotal * 100).toFixed(2)}% of total shipments</Typography.Text>
             </Space>
         </ProCard>
+        <ProCard
+            {...commonConfig}
+            title={<Typography.Text type='danger'>WARNGINGS</Typography.Text>}
+            layout='center'
+            extra={<Link to={{
+                pathname: '/transport/v2/list',
+                search: `?begin_date=${analysisReq.begin_date.format(dateFormat)}&end_date=${analysisReq.end_date.format(dateFormat)}`
+            }}><RightOutlined /></Link>}
+        >
+            <Space direction='vertical'>
+                <Typography.Text type='danger' style={{ fontSize: 40 }}>{warning}</Typography.Text>
+                {/* <Typography.Text type="secondary" style={{ visibility: 'hidden' }}> empty</Typography.Text> */}
+            </Space>
+        </ProCard>
 
         <ProCard
-            hoverable
+            {...commonConfig}
             title={<Typography.Text type="secondary">RETURNING TO SENDER</Typography.Text>}
             extra={<Link to={{
                 pathname: '/transport/v2/list',
@@ -59,7 +87,7 @@ const StatisticAnalysis: React.FC<StatisticAnalysisReq> = (props) => {
         </ProCard>
 
         <ProCard
-            hoverable
+            {...commonConfig}
             title={<Typography.Text type="secondary">RETURNED TO SENDER</Typography.Text>}
             extra={<Link to={{
                 pathname: '/transport/v2/list',
@@ -73,7 +101,7 @@ const StatisticAnalysis: React.FC<StatisticAnalysisReq> = (props) => {
         </ProCard>
 
         <ProCard
-            hoverable
+            {...commonConfig}
             title={<Typography.Text type="secondary">DELIVERED</Typography.Text>}
             extra={<Link to={{
                 pathname: '/transport/v2/list',
@@ -85,7 +113,7 @@ const StatisticAnalysis: React.FC<StatisticAnalysisReq> = (props) => {
                 <Typography.Text type="secondary">{(deliveredTotal / statisticTotal * 100).toFixed(2)}% of total shipments</Typography.Text>
             </Space>
         </ProCard>
-    </ProCard>)
+    </ProCard >)
 }
 
 export default StatisticAnalysis;
